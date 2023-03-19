d975df261daca92aef704617b0eee1f4bff8400f
brayan_jules <brayan_jules@epam.com>
2023-03-18 23:49:05 -0300

    fix: fixed the logic and added more test to increment the coverage and validate most of the scenarios.

--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -6,12 +6,47 @@ def test_skipped_stats():
     delta_table = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
     filters = [('a_float', '=', 4.5)]
     res = levi.skipped_stats(delta_table, filters)
-    expected = {'num_files': 2, 'num_files_skipped': 1, 'num_bytes_skipped': 996}
+    expected = {'num_files': 3, 'num_files_skipped': 2, 'num_bytes_skipped': 3887}
     assert res == expected
 
-    filters2 = [('a_float', '>', 1), ('a_float', "<", 3)]
-    res2 = levi.skipped_stats(delta_table, filters2)
-    expected2 = {'num_files': 2, 'num_files_skipped': 2, 'num_bytes_skipped': 1980}
+
+def test_skipped_stats_between():
+    delta_table = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
+    filters = [('a_float', '>', 1), ('a_float', "<", 3)]
+    res = levi.skipped_stats(delta_table, filters)
+    expected = {'num_files': 3, 'num_files_skipped': 1, 'num_bytes_skipped': 984}
+    assert res == expected
+
+
+def test_skipped_stats_less_than():
+    delta_table = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
+    filters = [('a_float', "<", 4.5)]
+    res2 = levi.skipped_stats(delta_table, filters)
+    expected2 = {'num_files': 3, 'num_files_skipped': 0, 'num_bytes_skipped': 0}
+    assert res2 == expected2
+
+
+def test_skipped_stats_less_than_or_equal():
+    delta_table = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
+    filters = [('a_float', "<=", 2.3)]
+    res2 = levi.skipped_stats(delta_table, filters)
+    expected2 = {'num_files': 3, 'num_files_skipped': 1, 'num_bytes_skipped': 984}
+    assert res2 == expected2
+
+
+def test_skipped_stats_greater_than():
+    delta_table = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
+    filters = [('a_float', ">", 4.5)]
+    res2 = levi.skipped_stats(delta_table, filters)
+    expected2 = {'num_files': 3, 'num_files_skipped': 2, 'num_bytes_skipped': 3887}
+    assert res2 == expected2
+
+
+def test_skipped_stats_greater_than_or_equal():
+    delta_table = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
+    filters = [('a_float', ">", 4.5)]
+    res2 = levi.skipped_stats(delta_table, filters)
+    expected2 = {'num_files': 3, 'num_files_skipped': 2, 'num_bytes_skipped': 3887}
     assert res2 == expected2
 
 
@@ -19,18 +54,15 @@ def test_filters_to_sql():
     assert levi.filter_to_sql(("a_float", "=", 4.5)) == "(`min.a_float` <= 4.5 and `max.a_float` >= 4.5)"
 
 
-
 def test_filter_to_sql():
     assert levi.filter_to_sql(("a_float", "=", 4.5)) == "(`min.a_float` <= 4.5 and `max.a_float` >= 4.5)"
-    assert levi.filter_to_sql(("a_float", ">", 3)) == "(`min.a_float` < 3)"
-
-
+    assert levi.filter_to_sql(("a_float", ">", 3)) == "(`max.a_float` > 3)"
 
 
 def test_delta_file_sizes():
     dt = DeltaTable("./tests/reader_tests/generated/basic_append/delta")
     res = levi.delta_file_sizes(dt, ["<300b", "300b-1kb", "1kb-100kb", ">100kb"])
-    expected = {'num_files_<300b': 0, 'num_files_300b-1kb': 2, 'num_files_1kb-100kb': 0, 'num_files_>100kb': 0}
+    expected = {'num_files_<300b': 0, 'num_files_300b-1kb': 2, 'num_files_1kb-100kb': 1, 'num_files_>100kb': 0}
     assert res == expected
 
 
